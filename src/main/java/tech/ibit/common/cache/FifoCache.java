3efdc3c746d6b6f0aee83b1b4004db0d12966dd5
ben <xiaobenma020@gmail.com>
2020-07-29 16:30:18 +0800

    增加备注信息

--- src/main/java/tech/ibit/common/cache/FifoCache.java
+++ src/main/java/tech/ibit/common/cache/FifoCache.java
@@ -9,6 +9,9 @@ public class FifoCache<K, V> extends MemCache<K, V> {
 
     /**
      * 构造FIFO缓存
+     *
+     * @param maxSize  缓存大小
+     * @param lifeTime 缓存时间（单位：毫秒）
      */
     public FifoCache(int maxSize, int lifeTime) {
         super(maxSize, lifeTime, false);
--- src/main/java/tech/ibit/common/cache/LruCache.java
+++ src/main/java/tech/ibit/common/cache/LruCache.java
@@ -7,8 +7,12 @@ package tech.ibit.common.cache;
  */
 public class LruCache<K, V> extends MemCache<K, V> {
 
+
     /**
      * 构造LRU缓存
+     *
+     * @param maxSize  缓存大小
+     * @param lifeTime 缓存时间（单位：毫秒）
      */
     public LruCache(int maxSize, int lifeTime) {
         super(maxSize, lifeTime, true);
